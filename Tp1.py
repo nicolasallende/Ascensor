4c740106788a29b3ada99d4ce9b6a61270a9b4cf
nicolas <allenden96@gmail.com>
2019-10-07 18:09:09 -0300

    se emprolijo un poco las cosas

--- Tp1.py
+++ Tp1.py
@@ -15,13 +15,13 @@ import numpy as np #Manejo de arrays
 import matplotlib.pylab as plt #Rutinas gráficas
 
 ##Constantes
-fuerza = 640 #Fuerza medida en newtons
+fuerza = 280 #Fuerza medida en newtons
 
 masa_cabina = 350 #peso cabina
 
 masa_persona = 75 #peso prom personas
 
-numero_personas = 3 #a determinar
+numero_personas = 6 #a determinar
 
 masa = masa_cabina + masa_persona*numero_personas
 
@@ -32,8 +32,8 @@ acel_maxima = fuerza/masa #aceleracion maxima posible
 tiempo_final = math.sqrt(18*masa/fuerza)
 
 tiempoEn30Porciento = ( (fuerza / masa) - acel_maxima * 0.3 ) * (masa/fuerza) * (tiempo_final / 2)
-print('El tiempo en 30 porciento es:',tiempoEn30Porciento)
-print('tiempo final', tiempo_final)
+
+
 print('-------------------------------------')
 print('La fuerza que voy a usar es ', fuerza, '[N]')
 print('------------------- ------------------')
@@ -57,11 +57,8 @@ def aceleracion(t):
     a = ( (fuerza/masa) * (1  - ((t*2) / tiempo_final)))
     return a
 
-print( tiempoEn30Porciento)
-X0 = posicion(tiempoEn30Porciento)
 
-
-#intento de grafico de posicion
+#grafico de posicion
 
 x = np.linspace(0, tiempo_final )
 y = posicion(x)
@@ -75,9 +72,9 @@ plt.title('Funcion de la posicion')
 plt.grid(True)
 plt.savefig('Grafico Posicion')
 
-#intento de grafico de velocidad
+#grafico de velocidad
 
-x = np.linspace(0, tiempo_final ) #!!!!aca hay que cambiar el tiempofinal por otra cosa creo
+x = np.linspace(0, tiempo_final )
 y = velocidad(x)
 plt.figure(figsize = (10,7))
 plt.plot(x, y)
@@ -89,7 +86,7 @@ plt.title('Funcion de la velocidad')
 plt.grid(True)
 plt.savefig('Grafico Velocidad')
 
-#intento de grafico de aceleracion
+#grafico de aceleracion
 
 x = np.linspace(0, tiempo_final )
 y = aceleracion(x)
@@ -106,58 +103,64 @@ plt.savefig('Grafico Aceleracion')
 
 
 def f(x):
-    fa = posicion(x) - X0
+    fa = posicion(x) - posicion(tiempoEn30Porciento) #este es el X0
     df = velocidad(x)
     return fa, df
     
 TOL = 5E-16    # tolerancia
-MAXITER_B = 2  # biseccion   
-MAXITER_N = 60  # newthon
-a = 0
-b = tiempo_final
+MAXITER_B = 2  # maxima cantidad de iteraciones para biseccion
+MAXITER_N = 60  # maxima cantidad de iteraciones para newton
 
-formato_salida_N = '%03d %+.17E %+.3E %+.3E'#ver que es esto
-formato_salida_B = '%03d %+.17E %+.3E'
+def Biseccion(a,b):
+   formato_salida_B = '%03d %+.17E %+.3E'
+   fa = f(a)[0]
+   fb = f(b)[0]
+   print('-------------------------------------')
+   print("Método de bisección")
+   if abs(fa) < TOL:
+       xo = a
+       print(formato_salida_B % (0, xo, fa))
+   elif abs(fb) < TOL:
+        xo = b
+        print(formato_salida_B % (0, xo, fb))
+   elif fb * fa < 0:
+       k = 0
+       fin = False
+       while k < MAXITER_B and not fin:
+           xo = (a + b) / 2
+           f_val = f(xo)[0]
+           print(formato_salida_B % (k, xo, f_val))
+           k += 1
+           if abs(f_val) < TOL:
+               fin = True
+           elif f(a)[0] * f_val < 0:
+               b = xo
+           else:
+               a = xo
+   else:
+       print("El intervalo no contiene raíces")
+       xo = a
+   return xo
 
-fa = f(a)[0] 
-fb = f(b)[0]
-print("Método de bisección")
-if abs(fa) < TOL:
-    xo = a
-    print(formato_salida_B % (0, xo, fa))
-elif abs(fb) < TOL:
-    xo = b
-    print(formato_salida_B % (0, xo, fb))
-elif fb * fa < 0:
+
+
+
+def Newton():
+    xo = Biseccion(0,tiempo_final)
+    formato_salida_N = '%03d %+.17E %+.3E %+.3E'
+    print('-------------------------------------')
+    print("Método de Newton")
     k = 0
     fin = False
-    while k < MAXITER_B and not fin:
-        xo = (a + b) / 2
-        f_val = f(xo)[0]
-        print(formato_salida_B % (k, xo, f_val))
+    while k < MAXITER_N and not fin:
+        fo = f(xo)[0]
+        dfo = f(xo)[1]
+        xk = -fo / dfo + xo
+        e_u = (xo - xk) / xk
+        fin = abs(fo) < TOL and abs(e_u) < TOL
+        print(formato_salida_N % (k, xo, fo, e_u))
+        xo = xk
         k += 1
-        if abs(f_val) < TOL:
-            fin = True
-        elif f(a)[0] * f_val < 0:
-            b = xo
-        else:
-            a = xo
-else:
-    print("El intervalo no contiene raíces")
-    xo = a
+    return None
 
-print("Método de Newton")
-k = 0
-fin = False
-while k < MAXITER_N and not fin:
-    fo = f(xo)[0]
-    dfo = f(xo)[1]
-    xk = -fo / dfo + xo
-    e_u = (xo - xk) / xk
-    fin = abs(fo) < TOL and abs(e_u) < TOL
-    print(formato_salida_N % (k, xo, fo, e_u))
-    xo = xk
-    k += 1
-
-
-
+Newton()
